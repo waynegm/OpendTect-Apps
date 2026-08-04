ada2816810bd65084b040f2769d174f91c83314d
waynegm <wayne.mogg@gmail.com>
2026-08-04 11:04:13 +1000

    start colorbar work

--- src/shared/uiseisview.py
+++ src/shared/uiseisview.py
@@ -20,8 +20,23 @@ from shared.uiodbind import (
 )
 from shared.uitools import uiLabelledComboBox, uiSpinBoxRowWidget
 
+def cmap_fault_red():
+    return pg.ColorMap(
+        [0.0,0.5,1.0],
+        [(255,255,255),(255,255,255),(255,0,0)]
+    )
 
+def cmap_dgb_fault():
+    return pg.ColorMap(
+        [0.0,0.4,0.49,0.58,0.85,1.0],
+        [(255,255,255),(197,180,153),(216,173,1),(73,145,9),(0,85,0),(0,111,166)]
+    )
 class uiSeisView(QWidget):
+    cmaps = {
+        "Fault-Red": cmap_fault_red(),
+        "DGB_Fault": cmap_dgb_fault(),
+    }
+
     def __init__(self):
         super().__init__()
         self._layout = QVBoxLayout()
@@ -31,6 +46,8 @@ class uiSeisView(QWidget):
         self.overseis = Seismic3DSel("Overlay", above=False)
         self.baseseis.setSurvey(self.surveysel.currentText())
         self.overseis.setSurvey(self.surveysel.currentText())
+        self.overseis_cmap_sel = uiLabelledComboBox(label="", above=False)
+        self.overseis_cmap_sel.addItems(list(self.cmaps.keys()))
         self.slicetype = uiLabelledComboBox(label="", above=False)
         self.slicetype.addItems(["Inline", "Crossline", "Z"])
         self.slicesel = uiSpinBoxRowWidget(prefix=[""], label="Slice", above=False, withsym=False)
@@ -38,6 +55,7 @@ class uiSeisView(QWidget):
         self.controls_layout.addWidget(self.surveysel)
         self.controls_layout.addWidget(self.baseseis)
         self.controls_layout.addWidget(self.overseis)
+        self.controls_layout.addWidget(self.overseis_cmap_sel)
         self.controls_layout.addWidget(self.slicetype)
         self.controls_layout.addWidget(self.slicesel)
 
@@ -50,39 +68,28 @@ class uiSeisView(QWidget):
         )
         self.baseseis_item = pg.ImageItem()
         self.overseis_item = pg.ImageItem()
-#        self.overseis_item.setOpacity(0.25)
+        self.overseis_item.setOpacity(0.25)
         self.seis_view.addItem(self.baseseis_item)
         self.seis_view.addItem(self.overseis_item)
-#        self.seis_view.addColorBar(self.baseseis_item,colorMap='CET-L2')
         self.baseseis_cmap = pg.ColorBarItem(colorMap='CET-L2')
         self.baseseis_cmap.setImageItem(self.baseseis_item)
-        over_lut = np.zeros((256, 4), dtype=np.uint8)
-        over_lut[:, 0] = np.arange(256)
-        over_lut[:, 1] = 0
-        over_lut[:, 2] = 0
-        over_lut[0, 3] = 0
-        over_lut[1:, 3] = 200
-        self.overseis_item.setLookupTable(over_lut)
-        self.overseis_item.setLevels([0.5,1.0])
-#        self.overseis_cmap = pg.ColorBarItem(colorMap='CET-L18')
-#        self.overseis_cmap.setImageItem(self.overseis_item)
         self.seismic_layout.addItem(self.baseseis_cmap)
-#        self.seismic_layout.addItem(self.overseis_cmap)
-        #self.overseis_item = pg.ImageItem()
+        self.overseis_cmap = pg.ColorBarItem(colorMap=self.cmaps['Fault-Red'])
+        self.overseis_cmap.setImageItem(self.overseis_item)
+        self.seismic_layout.addItem(self.overseis_cmap)
 
         self._layout.addLayout(self.controls_layout)
         self._layout.addWidget(self.seismic_layout)
         self.setLayout(self._layout)
-        #self.seis_view.addItem(self.overseis_item)
         self._on_survey_change()
+        self._on_overseis_cbar_change()
         self.surveysel.currentTextChanged.connect(self._on_survey_change)
         self.baseseis.currentTextChanged.connect(self._on_data_change)
         self.overseis.currentTextChanged.connect(self._on_data_change)
         self.slicetype.currentTextChanged.connect(self._on_slicetype_change)
+        self.overseis_cmap_sel.currentTextChanged.connect(self._on_overseis_cbar_change)
         self.slicetype.currentTextChanged.connect(self.update_view)
         self.slicesel.valuesChanged.connect(self.update_view)
-        # Enable Alpha Blending for Overlay
-        #self.overseis_item.setOpacity(0.5)
 
     def _on_survey_change(self):
         surveynm = self.surveysel.currentText()
@@ -102,6 +109,14 @@ class uiSeisView(QWidget):
         self.slicesel.set_range(-1, ranges[slicetype][0], ranges[slicetype][1], ranges[slicetype][2])
         self.slicesel.set_values([(ranges[slicetype][0]+ranges[slicetype][1])//2])
         self.update_view()
+
+    def _on_overseis_cbar_change(self):
+        self.seismic_layout.removeItem(self.overseis_cmap)
+        cmap = self.overseis_cmap_sel.currentText()
+        self.overseis_cmap = pg.ColorBarItem(colorMap=self.cmaps[cmap])
+        self.seismic_layout.addItem(self.overseis_cmap)
+        self.overseis_cmap.setImageItem(self.overseis_item)
+#        self.update_view()
 
     def update_view(self):
         ranges = self._baseseismic.ranges
